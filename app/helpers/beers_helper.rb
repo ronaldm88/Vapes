02a8ceef40bfd36313159c99dd8b913c62abcf75
zacscodingclub <zbaston@gmail.com>
2016-07-03 11:47:11 -0500

    basic beer new errors finished

--- app/helpers/beers_helper.rb
+++ app/helpers/beers_helper.rb
@@ -3,4 +3,15 @@ module BeersHelper
   def display_beers(beers)
 
   end
+
+  def beer_error_messages(beer)
+    binding.pry
+    if beer.errors.any?
+      content_tag :h2, "We couldn't add the beer because it's missing some required info."
+
+      beer.errors.full_messages.each do |message|
+        content_tag :div, "#{message}<br>", class:"fields_with_errors"
+      end
+    end
+  end
 end
