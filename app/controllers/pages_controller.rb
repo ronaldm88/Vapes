4db2e1f055281cad9bb041d7dd46a08eecc849bf
zacscodingclub <zbaston@gmail.com>
2016-07-18 17:52:44 -0500

    add feed route/controller action/view

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,11 +2,15 @@ class PagesController < ApplicationController
   def index
     if user_signed_in?
 
-      redirect_to beers_path
+      redirect_to check_ins_path
     else
       @user = User.new
       render :index
     end
+  end
+
+  def feed
+
   end
 
   def about
