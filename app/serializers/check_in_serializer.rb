84e08be9f9d0559a0e9b4d3523ef3b554756624a
zacscodingclub <zbaston@gmail.com>
2016-07-18 19:01:48 -0500

    nested feed in place.  updated to-do

--- app/serializers/check_in_serializer.rb
+++ app/serializers/check_in_serializer.rb
@@ -1,3 +1,5 @@
 class CheckInSerializer < ActiveModel::Serializer
   attributes :id, :user_id, :beer_id, :rating, :comment, :created_at
+  has_one :user, serializer: CheckInUserSerializer
+  has_one :beer, serializer: CheckInBeerSerializer
 end
