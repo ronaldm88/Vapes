3b05e5acda68c44ee3423629deba1a5b8524d03e
zacscodingclub <zbaston@gmail.com>
2016-07-20 22:12:19 -0500

    playing with the polling...

--- app/assets/javascripts/check_ins.js
+++ app/assets/javascripts/check_ins.js
@@ -18,6 +18,8 @@ function addCheckInSubmitListener() {
 
 function attachListeners() {
   addCheckInUIListener();
+
+  setInterval(function(){ getCheckIns(); }, 15000 );
 }
 
 // UI Manipulation
@@ -51,27 +53,35 @@ function getCheckIns() {
 }
 
 // Manual Labor
+function filterCheckIn(data, id) {
+  return data.map(function(checkIn) {
+    if (checkIn.id > id) {
+      return checkIn;
+    }
+  }).filter(function(n) { return n != undefined });
+}
+
+function buildCheckInsList(checkIns) {
+  return checkIns.map(function(checkIn) {
+    var listItem = '<li id="' + checkIn.id;
+    listItem += '"><a href="/users/' + checkIn.user.id;
+    listItem += '/check_ins/' + checkIn.id
+    listItem += '">' + checkIn.rating + '/10 - ' + checkIn.beer.name;
+    listItem += ' by ' + checkIn.user.username + ', just now!';
+    listItem += '</a></li>';
+
+    return listItem;
+  });
+}
+
 function processCheckIns(data) {
   var prevLastCheckIn = parseInt($('.check-ins-list li a').first().attr("id"));
   var dataLastCheckIn = data[0].id;
 
   if (prevLastCheckIn < dataLastCheckIn) {
-    var newCheckIns = data.map(function(checkIn) {
-      if (checkIn.id > prevLastCheckIn) {
-        return checkIn;
-      }
-    }).filter(function(n) { return n != undefined });
+    var newCheckIns = filterCheckIn(data, prevLastCheckIn);
 
-    var newItems = newCheckIns.map(function(checkIn) {
-      var listItem = '<li id="' + checkIn.id;
-      listItem += '"><a href="/users/' + checkIn.user.id;
-      listItem += '/check_ins/' + checkIn.id
-      listItem += '">' + checkIn.rating + '/10 - ' + checkIn.beer.name;
-      listItem += ' by ' + checkIn.user.username + ', just now!';
-      listItem += '</a></li>';
-
-      return listItem;
-    });
+    var newItems = buildCheckInsList(newCheckIns);
 
     $('.check-ins-list ul').prepend(newItems);
     resetForm();
@@ -84,5 +94,4 @@ function processCheckIns(data) {
 
 $(function() {
   attachListeners();
-  setInterval(function(){ getCheckIns(); }, 5000 );
 });
